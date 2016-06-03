2acdd70f848dc65fd8ce23bd1fe2f0822ce37cb7
Marco Bueno <bueno.marco@gmail.com>
2016-06-03 09:32:41 -0400

    [profile] Adicionado modelo no ORM

---
+++ Projects/Blink/libs/orm/profile.cpp
@@ -0,0 +1,45 @@
+#include "profile.h"
+
+profileList* profile::m_allList = NULL;
+
+MODEL_BEGIN_MAP(profile)
+     MODEL_MAP_PRIMARYKEY(id, "id");
+     MODEL_MAP_FIELD(name, "name");
+     MODEL_MAP_FIELD(usepreaut, "usepreaut");
+     MODEL_MAP_FIELD(usevisit, "usevisit");
+     MODEL_MAP_FIELD(usedweller, "usedweller");
+     MODEL_MAP_FIELD(usevisitant, "usevisitant");
+     MODEL_MAP_FIELD(useuser, "useuser");
+     MODEL_MAP_FIELD(useprofile, "useprofile");
+MODEL_END_MAP()
+
+QList<profile*>* profile::findAll()
+{
+    MODEL_INIT_LIST(profile, m_allList);
+
+    QString query = QString("select * from profile");
+
+    if (!profile::fillModelList(m_allList, query))
+        return NULL;
+
+    return m_allList;
+}
+
+profile* profile::findByid(int id)
+{
+    profile *profileM = new profile();
+
+    QString query = QString("select * from profile where id = %3").arg(id);
+
+    if (!profileM->fillModelFromQuery(query))
+    {
+        delete profileM;
+        return NULL;
+    }
+
+    return profileM;
+}
+profile* profile::findByid(int id, QString database)
+{
+    return profile::findByPrimaryKey(id, database);
+}
---
+++ Projects/Blink/libs/orm/profile.h
@@ -0,0 +1,32 @@
+#ifndef profile_H
+#define profile_H
+ 
+#include "model.h"
+
+DECLARE_MODEL(profile)
+
+class profile : public Model
+{
+
+
+     MODEL_DECLARE_ATTRIBUTE(int, id);
+     MODEL_DECLARE_ATTRIBUTE(QString, name);
+     MODEL_DECLARE_ATTRIBUTE(bool, usepreaut);
+     MODEL_DECLARE_ATTRIBUTE(bool, usevisit);
+     MODEL_DECLARE_ATTRIBUTE(bool, usedweller);
+     MODEL_DECLARE_ATTRIBUTE(bool, usevisitant);
+     MODEL_DECLARE_ATTRIBUTE(bool, useuser);
+     MODEL_DECLARE_ATTRIBUTE(bool, useprofile);
+
+
+     MODEL_MATCH_TABLE(profile, "profile")
+
+private:
+    static profileList* m_allList;
+public:
+    static profileList* findAll(void);
+    static profile* findByid(int id);
+    static profile* findByid(int id, QString database);
+};
+ 
+#endif 

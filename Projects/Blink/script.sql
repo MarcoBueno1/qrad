e156fc217c020308dbdff3d674dcbd7c84c96ca0
Marco Bueno <bueno.marco@gmail.com>
2017-01-10 11:06:15 -0400

    Script para criacao da tabela de login / profile e insert do login/profile default
    Signed-off-by: Marco Bueno <bueno.marco@gmail.com>

---
+++ Projects/Blink/script.sql
@@ -0,0 +1,38 @@
+create table profile
+(
+   id serial,
+   name character varying,
+   usepreaut boolean,
+   usevisit boolean,
+   usedweller boolean,
+   usevisitant boolean,
+   useuser boolean,
+   useprofile boolean,
+   ConfigureEmail boolean,
+   tp integer,
+   removed boolean
+);
+
+ALTER TABLE ONLY profile
+    ADD CONSTRAINT profile_key PRIMARY KEY (id);
+
+insert into profile( id, name, usepreaut, usevisit, usedweller, usevisitant, useuser, useprofile, configureemail, tp, removed ) values ( 1, 'ADMINISTRADOR', true, true, true, true, true, true, true, 1, false );
+
+
+
+create table vuser
+(
+   id serial,
+   name character varying,
+   email character varying,
+   contractsince date,
+   password character varying,
+   profile integer,
+   loid    integer,
+   removed boolean
+);
+
+ALTER TABLE ONLY vuser
+    ADD CONSTRAINT vuser_key PRIMARY KEY (id);
+
+insert into vuser ( id, email, password, profile ) values ( 1, 'marco','1ac61befdde2a266f91b6088478c535e', 1);

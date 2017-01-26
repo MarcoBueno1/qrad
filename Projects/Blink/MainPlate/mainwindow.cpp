d6f23473f244f54d0967a700315126348dd75b95
Marco Bueno <bueno.marco@gmail.com>
2017-01-25 21:39:56 -0400

    [MainPlate] Versao minimamente funcional

--- Projects/Blink/MainPlate/mainwindow.cpp
+++ Projects/Blink/MainPlate/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QProcess>
 #include <QMessageBox>
+#include <QCameraInfo>
 
 #define CMD_DBD "c:\\dvl\\test_ocr\\openalpr_32\\alpr.exe -c eu \"c:\\Program Files (x86)\\Tesseract-OCR\\placa.jpg\""
 #define CMD_CASA "C:\\Dvl\\ocr\\openalpr_64\\alpr.exe -c eu \"c:\\Program Files (x86)\\Tesseract-OCR\\placa.jpg\""
@@ -11,59 +12,127 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    m_camera = NULL;
+    m_timer= NULL;
+    m_bSave = true;
+
+    if(!QCameraInfo::availableCameras().count())
+        QMessageBox::warning(this, "Oops!", "Não foram encontradas cameras conectadas a este computador");
+    else
+    {
+        m_FullPath = "C:\\Dvl\\qrad\\bin\\image.jpeg";
+        m_camera = new QCamera(QCameraInfo::defaultCamera()) ;
+        m_imageCapture = new QCameraImageCapture(m_camera);
+
+        connect(m_camera, SIGNAL(stateChanged(QCamera::State)), this, SLOT(updateCameraState(QCamera::State)));
+        connect(m_camera, SIGNAL(error(QCamera::Error)), this, SLOT(displayCameraError()));
+
+        connect(m_imageCapture, SIGNAL(readyForCaptureChanged(bool)), this, SLOT(readyForCapture(bool)));
+        connect(m_imageCapture, SIGNAL(imageCaptured(int,QImage)), this, SLOT(processCapturedImage(int,QImage)));
+        connect(m_imageCapture, SIGNAL(imageSaved(int,QString)), this, SLOT(imageSaved(int,QString)));
+        connect(m_imageCapture, SIGNAL(error(int,QCameraImageCapture::Error,QString)), this,
+                SLOT(displayCaptureError(int,QCameraImageCapture::Error,QString)));
+
+        m_camera->setViewfinder(ui->viewfinder);
+        m_camera->start();
+        m_timer = new QTimer;
+        m_timer->setInterval(500);
+        connect(m_timer, SIGNAL(timeout()), this, SLOT(timeout()));
+        m_timer->start();
+
+    }
+
+}
+
+void MainWindow::timeout()
+{
+    m_imageCapture->capture();
+}
+
+void MainWindow::updateCameraState(QCamera::State state)
+{
+    switch (state) {
+    case QCamera::ActiveState:
+        //ui->actionStartCamera->setEnabled(false);
+       // ui->actionStopCamera->setEnabled(true);
+       // ui->captureWidget->setEnabled(true);
+        //ui->actionSettings->setEnabled(true);
+        break;
+    case QCamera::UnloadedState:
+    case QCamera::LoadedState:
+       // ui->actionStartCamera->setEnabled(true);
+       // ui->actionStopCamera->setEnabled(false);
+//        ui->captureWidget->setEnabled(false);
+       // ui->actionSettings->setEnabled(false);
+        break;
+    }
+}
+
+void MainWindow::displayCameraError()
+{
+    QMessageBox::warning(this, tr("Camera error"), m_camera->errorString());
+}
+void MainWindow::readyForCapture(bool ready)
+{
+  //  ui->takeImageButton->setEnabled(ready);
+}
+
+void MainWindow::processCapturedImage(int requestId, const QImage& img)
+{
+    Q_UNUSED(requestId);
+
+    if( m_bSave )
+    img.save(m_FullPath);
+    //accept();
+}
+
+void MainWindow::displayCaptureError(int id, const QCameraImageCapture::Error error, const QString &errorString)
+{
+    Q_UNUSED(id);
+    Q_UNUSED(error);
+    QMessageBox::warning(this, tr("Oops! Problema ao capturar imagem..."), errorString);
+    //isCapturingImage = false;
 }
 
 MainWindow::~MainWindow()
 {
+    if( m_timer )
+    {
+        m_timer->stop();
+        delete m_timer;
+    }
+    if( m_camera )
+        delete m_camera;
+
     delete ui;
 }
 QString MainWindow::FindPlate(QString path)
 {
+    m_bSave = false;
+    QString result = "NAO IDENTIFICADO";
     QProcess *process = new QProcess(this);
     QString program = QString("%1 %2").arg(CMD_CASA).arg(path);
     process->start(program);
-    process->waitForStarted();
-    process->waitForFinished();
-    process->waitForReadyRead();
-    QStringList data = QString(process->readAll()).split("\n");
-    //if(!data.count())
-//        QMessageBox::information(this,"Result", data.join());
-//    else
-        QMessageBox::information(this,"Placa:", data.at(1));
-
-
-    /*
-
-
-
-
-   alpr.exe -c eu "c:\Program Files (x86)\Tesseract-OCR
-\placa.jpg" > result.txt
-
-    QLibrary wrp("c:\\dvl\\qrad\\bin\\OpenalprWrapper");
-    typedef int (*Find)(char *, char *);
-
-    if( !wrp.load() )
-        QMessageBox::information(this, "Nao pude carregar a DLL", "load");
-
-    Find fnFind = (Find) wrp.resolve("Find");
-    if (!fnFind)
+    process->waitForStarted(3000);
+    process->waitForFinished(3000);
+    process->waitForReadyRead(3000);
+    QString stRead = QString(process->readAll());
+    if( stRead.length() )
     {
-        QMessageBox::information(this, "Nao pude resolver a DLL", "Error");
-        return "";
+         QStringList data = stRead.split("\n");
+         result = data.at(1);
     }
 
-    char szPlate[1024];
-    if( fnFind(path.toLatin1().data(), szPlate))
-        return QString(szPlate);
-
-    return "";
-    */
+    m_bSave = true;
+    return result;
 }
 
 void MainWindow::on_pushButton_clicked()
 {
-    FindPlate("C:\\Dvl\\ocr\\openalpr_64\\WhatsApp.jpeg");
+    QString plate = FindPlate("C:\\Dvl\\qrad\\bin\\image.jpeg"/*"C:\\Dvl\\ocr\\openalpr_64\\WhatsApp.jpeg"*/);
 
-//    QMessageBox::information(this, "Retornado", QString(plate));
+    plate.truncate(plate.indexOf("confidence"));
+    plate.remove(plate.indexOf("-"),1);
+    plate = plate.trimmed();
+    QMessageBox::information(this, "Retornado", QString(plate));
 }
--- Projects/Blink/MainPlate/mainwindow.h
+++ Projects/Blink/MainPlate/mainwindow.h
@@ -2,6 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QCamera>
+#include <QCameraImageCapture>
+#include <QTimer>
 
 namespace Ui {
 class MainWindow;
@@ -15,12 +18,24 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void readyForCapture(bool ready);
+    void processCapturedImage(int requestId, const QImage &img);
+    void displayCaptureError(int id, const QCameraImageCapture::Error error, const QString &errorString);
+    void updateCameraState(QCamera::State state);
+    void displayCameraError();
+    void timeout();
 private slots:
     void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
     QString FindPlate(QString path);
+    QCamera *m_camera;
+    QCameraImageCapture *m_imageCapture;
+    QString m_FullPath;
+    QTimer *m_timer;
+    bool m_bSave;
 };
 
 #endif // MAINWINDOW_H

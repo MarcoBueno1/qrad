ac375d9b6293ec5d948f9b254427c82edcb157b8
Marco Bueno <bueno.marco@gmail.com>
2017-10-03 13:59:41 -0400

    [EditMetreage] Corrigida a edicao.

--- Projects/Blink/libs/orm/metreage.cpp
+++ Projects/Blink/libs/orm/metreage.cpp
@@ -3,7 +3,7 @@
 
 ORM_BEGIN_MAP(metreage)
      ORM_MAP_PRIMARYKEY(id, "id");
-     ORM_MAP_FIELD(Descricao, "description");
+     ORM_MAP_FIELD(Descricao, "descricao");
      ORM_MAP_FIELD(m2, "m2");
      ORM_MAP_FIELD(MontlyValue, "montlyvalue");
 ORM_END_MAP()

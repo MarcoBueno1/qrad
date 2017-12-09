ca4e124180e50acf7fad4f90521268e9b22ac69f
Marco Bueno <bueno.marco@gmail.com>
2017-12-08 21:20:49 -0400

    [versao 14]

--- Projects/Blink/Main/mainwindow.cpp
+++ Projects/Blink/Main/mainwindow.cpp
@@ -7,7 +7,7 @@
 #include "qradconfig.h"
 #include "qradpluginaction.h"
 
-#define BLINK_RELEASE "1.0.13"
+#define BLINK_RELEASE "1.0.14"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
